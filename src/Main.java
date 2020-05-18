4a003fd4cb15898534fc5c1875629f50cdbd85c3
Tetiana Hrechkina <ttanya1476@gmail.com>
2020-05-18 12:19:08 +0300

    del class

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
 import static java.lang.Integer.parseInt;
+import static java.lang.Math.sqrt;
 
 public class Main {
     public static void main(String[] args) {
         try {
             int num = parseInt(args[0]);
-            Numerical number = new Numerical(num);
-            number.numbers();
-        }catch (Exception ex){
+            int result = (int) sqrt(num);
+            for (int i = 1; i < result; i++) {
+                System.out.print(i + ", ");
+            }
+        } catch (Exception ex) {
             System.out.println("Please enter the number");
             System.exit(1);
         }
